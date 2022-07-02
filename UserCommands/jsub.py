b6d4745bf5b3817ebc094ce86b2a7a420682f892
007Nil <sagniksarkar@ymail.com>
2022-07-03 02:40:45 +0530

    Added qstat.py and start working on jsub script

--- UserCommands/jsub.py
+++ UserCommands/jsub.py
@@ -1,1 +1,17 @@
 # Script for submitting jobs
+from ntpath import join
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Program to submit jobs to Simple Job Scheduler')
+    parser.add_argument("--queue","-q",help="name you the job where jobs will be submited")
+    parser.add_argument("--resources","-l",help="Add SJS resources (ncpus,mem, etc...)")
+    namespace, extra = parser.parse_known_args()
+
+    job_command = " ".join(extra)
+    print(namespace.queue)
+    # print(" ".join(args.binary))
+
+if __name__ == "__main__":
+    main()
